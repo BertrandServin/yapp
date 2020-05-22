7b3cf8951dbc9f87e14e039aec7620d152c08eba
Bertrand Servin <bertrand.servin@inra.fr>
2020-05-23 00:17:02 +0200

    setup script

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup( name = 'yapp',
+       version = '0.1',
+       description = 'Yet Another Phasing Project',
+       long_description = readme(),
+       url = 'https://github.com/BertrandServin/yapp',
+       author = 'Bertrand Servin',
+       author_email = 'bertrand.servin@inrae.fr',
+       license = 'LGPLv3',
+       packages = ['yapp'],
+       install_requires = [
+           'numpy',
+           'cyvcf2',
+           'fastphase'
+           ],
+       scripts=['bin/fphtrain'],
+       zip_safe = False)
+       
