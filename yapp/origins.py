8f5da56f4784b60b045b028fdfc7aa5a62369980
Bertrand Servin <bertrand.servin@inra.fr>
2021-04-07 09:22:56 +0200

    Add log info on GRM computation

--- yapp/origins.py
+++ yapp/origins.py
@@ -118,6 +118,7 @@ class OriginTracer():
         Ltot = 0
         GRM = np.zeros((N, N), dtype=np.float)
         for reg in phaser.regions:
+            logger.info(f"\tAccumulate region {reg}")
             origins = np.array(phaser.data[f'linkage/{reg}/origins'])
             L = origins.shape[2]
             compute_grm(N, L, origins, GRM)
