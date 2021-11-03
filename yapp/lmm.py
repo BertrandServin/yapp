8580b8ed19ab3a6a960c299b7b6e992ecf1137c4
Bertrand Servin <bservin@toulouse.inra.fr>
2021-11-03 13:34:32 +0100

    Correct implementation of the diagonal REML

--- yapp/lmm.py
+++ yapp/lmm.py
@@ -18,7 +18,10 @@ specific models used in yapp. This is *not* the most computationaly
 efficient module to do general mixed-model analysis.
 """
 
+from functools import partial
 import numpy as np
+from scipy.optimize import minimize
+from scipy.special import expit
 
 def relik_animal_model(h, Y, X, Gi):
     """
@@ -73,6 +76,38 @@ def relik_Gdiag(h, Y, X, diagG):
     ldetVi = np.sum(np.log(1 / (1+ diagG * h/(1-h))))
     return -0.5 * (ldetC - ldetVi + N * np.log(yPy))
 
+def relik_Gdiag_correct(h, Y, X, diagG):
+    """Computes the restricted likelihood of the animal model when G
+    is diagonal
+    
+    Y = XB + w + e
+
+    w ~ N(0, diagG ve.h/(1-h))
+    e ~ N(0, I ve)
+    
+    This is obtained from the full matrix model by diagonalization of
+    G through eigen decomposition and projection of the model on the
+    PCs. 
+
+    V(Y) is diagonal (I + h/(1-h) diagG) and easy to
+    invert. So the restricted likelihood can be obtained as :
+
+    C = Xt.Vi.X
+    CB = Xt.Vi.Y
+
+    L = P(Y|X,h) = \int P(Y|X,h,ve)P(ve) with P(ve) \propto 1/ve
+    logL = cste - 0.5 ( logdet(C) + N log(Yt.Vi.Y - Bt.C.B) )
+    """
+    N = Y.shape[0]
+    P = X.shape[1]
+    Vi = np.diag(1 / (1+ diagG * h/(1-h)))
+    C = X.T @ Vi @ X
+    B = np.linalg.solve(C,X.T @ Vi @ Y)
+    yPy = Y.T@Vi@Y - B.T@C@B
+    _,ldetC = np.linalg.slogdet(C)
+    ldetVi = np.sum(np.log(1 / (1+ diagG * h/(1-h))))
+    return -0.5 * (ldetC - ldetVi + (N-P) * np.log(yPy))
+
 def relik_lowrank(h, Y, X, eigVal, eigVec, valmin=1):
     """Computes the restricted likelihood of the animal model when G
     is appromixated by (eigVec.sqrt(lambda))(eigVec.sqrt(lambda))'
@@ -99,8 +134,8 @@ def relik_lowrank(h, Y, X, eigVal, eigVec, valmin=1):
     L = eigVal[eigVal>valmin]
     W  = eigVec[:,eigVal>valmin]
     K = W.shape[1]
-    print("*** K=", K)
-    print(min(eigVal),max(eigVal))
+    # print("*** K=", K)
+    # print(min(eigVal),max(eigVal))
     ## approximate inverse(V) using low rank matrix (Mischenko et al. 2007)
     LRD = np.diag(1/(vg + 1/L))
     Vi = np.eye(N) - vg * W@LRD@W.T
@@ -144,35 +179,54 @@ if __name__=='__main__':
     '''Simulate stuff to test funcs'''
 
     rng = np.random.default_rng()
-    Nfam = 30
-    Nind = 100
+    Nfam = 200
+    Nind = 10
     N = Nfam * Nind
-    h = 0.8
-    genetic = True
-    if genetic:
-        ## 1. Nfam families of halfsibs
-        G = np.kron(np.eye(Nfam),np.ones((Nind,Nind))*0.25)
-        np.fill_diagonal(G,1)
-        dG,U = np.linalg.eigh(G)
-        Zu = rng.multivariate_normal(mean = np.zeros(N),cov=G*h/(1-h))
-    else:
-        ## 2. random vectors
-        Z = rng.normal(size=(N,N))
-        dG,U = np.linalg.eigh(Z@Z.T)
-        u = rng.normal(size=N,scale=np.sqrt(h/(1-h)))
-        Zu = Z@u
-
-    ## Fit REML
-    X = np.block([[np.ones(N)],[np.random.randint(0,2,N)]]).T
-    beta = np.array([2,0.01])
-    Y = X@beta + Zu + np.random.randn(N)
-    hh = np.linspace(0.01,0.99)
+    nsim = 100
+    G = np.kron(np.eye(Nfam),np.ones((Nind,Nind))*0.25)
+    np.fill_diagonal(G,1)
+    dG,U = np.linalg.eigh(G)
     with open('../test/h.txt','w') as fout:
-        print('hh ll.lr ll.diag',file=fout)
-        Ytilde = U.T@Y
-        print("problem size", Ytilde.shape)
-        for x in hh:
-            res = relik_lowrank(x, Y, X, dG, U)
-            res2 = relik_Gdiag(x, U.T@Y, U.T@X, dG)
-            print(np.round(x,2), res,res2,file=fout)
+        print('h hreml success ll se',file=fout)
+        for isim in range(nsim):
+            h = rng.random()
+            Zu = rng.multivariate_normal(mean = np.zeros(N),cov=G*h/(1-h))
+            X = np.block([[np.ones(N)],[np.random.randint(0,2,N)]]).T
+            beta = np.array([2,0.01])
+            Y = X@beta + Zu + np.random.randn(N)
+            mylik = partial(relik_Gdiag_correct, Y=U.T@Y, X=U.T@X, diagG=dG)
+            optlik = lambda x: -mylik(x)
+            hmax = minimize(optlik,[0.4],bounds=[(1e-3,1-1e-3)], options={"disp":True})
+            print(np.round(h,3), np.round(hmax.x[0],3), hmax.success, -hmax.fun, np.sqrt(hmax.hess_inv.todense()[0,0]),file=fout)
+
+    # h = 0.5
+    # genetic = True
+    # if genetic:
+    #     ## 1. Nfam families of halfsibs
+    #     G = np.kron(np.eye(Nfam),np.ones((Nind,Nind))*0.25)
+    #     np.fill_diagonal(G,1)
+    #     dG,U = np.linalg.eigh(G)
+    #     Zu = rng.multivariate_normal(mean = np.zeros(N),cov=G*h/(1-h))
+    # else:
+    #     ## 2. random vectors
+    #     Z = rng.normal(size=(N,N))
+    #     dG,U = np.linalg.eigh(Z@Z.T)
+    #     u = rng.normal(size=N,scale=np.sqrt(h/(1-h)))
+    #     Zu = Z@u
+
+    # ## Fit REML
+    # X = np.block([[np.ones(N)],[np.random.randint(0,2,N)]]).T
+    # beta = np.array([2,0.01])
+    # Y = X@beta + Zu + np.random.randn(N)
+    # hh = np.linspace(0.01,0.9999)
+    # with open('../test/h.txt','w') as fout:
+    #     print('hh ll.diag',file=fout)
+    #     mylik = partial(relik_Gdiag_correct, Y=U.T@Y, X=U.T@X, diagG=dG)
+    #     optlik = lambda x: -mylik(x)
+    #     hmax = minimize(optlik,[0.4],bounds=[(1e-3,1-1e-3)], options={"disp":True})
+    #     print(f" h2 = {hmax.x}, success = {hmax.success}")
+    #     print(np.round(hmax.x[0],3), -hmax.fun, file=fout)
+    #     for x in hh:
+    #         res = relik_Gdiag_correct(x, U.T@Y, U.T@X, dG)
+    #         print(np.round(x,2), res,file=fout)
         
